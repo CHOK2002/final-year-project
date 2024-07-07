32d3245ff3dd8162a9494bc0e1bf3b996a9ac955
Joe <joechok@overwatchs.app>
2024-07-07 08:06:01 +0800

    initial commit

---
+++ Dataset/4 Data Preprocessing/emoticons_lib.py
@@ -0,0 +1,108 @@
+emoticons_lib = {
+    # Happy/Smiling
+    ":)": "smile",
+    ":-)": "smile",
+    ":]": "smile",
+    ":-]": "smile",
+    "=)": "smile",
+    ":D": "smile",
+    ":-D": "smile",
+    "=D": "smile",
+    "^_^": "smile",
+    ":')": "smile",
+    ":-))": "smile",
+    ":))": "smile",
+
+    # Sad/Frowning
+    ":(": "sad",
+    ":-(": "sad",
+    ":[": "sad",
+    ":-[": "sad",
+    "='(": "sad",
+    ":'(": "sad",
+    ":-'(": "sad",
+    "T_T": "sad",
+    ";-;": "sad",
+    ":<": "sad",
+    ":-<": "sad",
+
+    # Winking
+    ";)": "wink",
+    ";-)": "wink",
+    "*-)": "wink",
+
+    # Surprised/Shocked
+    ":O": "surprised",
+    ":-O": "surprised",
+    ":o": "surprised",
+    ":-o": "surprised",
+    ":-0": "surprised",
+    "=O": "surprised",
+
+    # Tongue Out
+    ":P": "tongue_out",
+    ":-P": "tongue_out",
+    ":p": "tongue_out",
+    ":-p": "tongue_out",
+    "=P": "tongue_out",
+    ":-Þ": "tongue_out",
+
+    # Confused
+    ":/": "confused",
+    ":-/": "confused",
+    ":\\": "confused",
+    ":-\\": "confused",
+    ":S": "confused",
+    ":-S": "confused",
+    ":|": "confused",
+
+    # Angry/Annoyed
+    ">:(": "angry",
+    ">:-(": "angry",
+    "D:<": "angry",
+
+    # Kissing
+    ":*": "kiss",
+    ":-*": "kiss",
+    ":x": "kiss",
+    ":-x": "kiss",
+
+    # Cool/Sunglasses
+    "8-)": "cool",
+    "B-)": "cool",
+    "8-|": "cool",
+    "B-|": "cool",
+
+    # Crying
+    ";_;": "cry",
+    ";-;": "cry",
+    "T_T": "cry",
+    ":'(": "cry",
+    "Q_Q": "cry",
+
+    # Laughing
+    "xD": "laugh",
+    "XD": "laugh",
+    ":')": "laugh",
+    "=D": "laugh",
+
+    # Heart/Love
+    "<3": "heart",
+    "</3": "broken_heart",
+
+    # Indifferent/Bored
+    ":|": "indifferent",
+    ":-|": "indifferent",
+
+    # Sleepy/Tired
+    "-_-": "sleepy",
+    "-__-": "sleepy",
+    "( ._.)": "sleepy",
+
+    # Nervous/Anxious
+    ">_<": "nervous",
+    ">.<": "nervous",
+    "o.o": "nervous",
+    "o_O": "nervous",
+    "O_o": "nervous"
+}
